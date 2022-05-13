e826a2184fca8142b7e6592867f78d416b527e5c
WriterV2 <primary-v2@protonmail.com>
2022-05-13 10:09:33 +0200

    Add highlighting for current linenumber

--- editor/nvim/lua/settings.lua
+++ editor/nvim/lua/settings.lua
@@ -2,10 +2,12 @@ vim.opt.encoding = "utf-8"
 vim.opt.relativenumber = true
 vim.opt.laststatus = 2
 vim.opt.autoindent = true
-vim.opt.shiftwidth=4
-vim.opt.softtabstop=4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.linebreak = true
 vim.opt.termguicolors = true
 vim.cmd("colorscheme dogrun")
-vim.opt.completeopt = {"menuone", "noinsert", "noselect"}
+vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
+vim.opt.cursorline = true
+vim.cmd("highlight clear CursorLine")
