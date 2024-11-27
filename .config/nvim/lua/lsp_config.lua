35ed7d8553b556fa6a0ad278e373551a1b3e9d54
WriterV2 <primary-v2@protonmail.com>
2024-11-27 19:10:03 +0100

    Neovim: Add workaround for unhandled rust analyzer diagnostic

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -95,6 +95,16 @@ local on_attach = function(client, bufnr)
     })
 end
 
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
+
 -- Language Servers
 
 -- Rust
